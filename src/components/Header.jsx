b752e8ac9710f9e45fe58156b6235fecc87d8aed
Sahil Abbas <sa9300432@gmail.com>
2026-01-21 23:11:28 +0530

    fixed text aling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,10 @@ function Header() {
     <img
      className='mix-blend-darken w-full max-w-10   h-auto object-cover'
     src="https://static.vecteezy.com/system/resources/previews/028/561/891/non_2x/resume-dual-tone-icon-icon-for-your-website-mobile-presentation-and-logo-design-vector.jpg" alt="" />
-    <h1 className='text-xl font-semibold'> ResumeGen </h1>
+    <h1 className='text-xl font-semibold '> ResumeGen </h1>
     
-    <Link to={"/Hero"} className='ml-8 text-sm md:ml-200 text-gray-800 font-semibold hover:text-purple-800'>Hero</Link>
-    <Link to={"/Resume"} className='ml-6 text-sm text-gray-800 font-semibold hover:text-purple-800'>preview</Link>
+    <Link to={"/Hero"} className='ml-24 text-md md:ml-200 text-gray-800 font-semibold hover:text-purple-800'>Hero</Link>
+    <Link to={"/Resume"} className='ml-4 text-md text-gray-800 font-semibold hover:text-purple-800'>preview</Link>
     </div>
 
 
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -49,7 +49,7 @@ const domloadpdf = () => {
 
 <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4 sm:gap-12 mb-6 p-3">
   <div>
-   <h1 className="text-xl sm:text-2xl font-bold">{resume?.name}</h1>
+   <h1 className="text-xl sm:text-2xl font-bold font-sans">{resume?.name}</h1>
   </div>
 <div className="text-xs sm:text-sm">
   <p>address: {resume?.address}</p>
@@ -61,64 +61,64 @@ const domloadpdf = () => {
 
 
 <div className='mb-8'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='text-start py-2 px-3  rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className='text-md font-bold '>Profesional Summary-:</h1>
+  <h1 className='text-md font-bold leading-tight '>Profesional Summary-:</h1>
   </div>
   <p>A highly motivated and goal-oriented professional with a strong work ethic and the ability to adapt to fast-paced environments. Committed to contributing to organizational growth through my problem-solving skills and dedication. A quick learner and a team player seeking an opportunity to utilize my skills to achieve company objectives.</p>
 </div>
 
 <div className='mb-4'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className="text-sm  sm:text-md font-bold ">CAREER OBJECTIVE -:</h1>
+  <h1 className="text-sm  sm:text-md font-bold leading-tight">CAREER OBJECTIVE -:</h1>
   </div>
   <p className="leading-relaxed">{resume?.objective}</p>
 </div>
 
 <div className='mb-4'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className="text-sm sm:text-md font-bold ">EDUCATION QALIFICATINS -:</h1>
+  <h1 className="text-sm sm:text-md font-bold leading-tight">EDUCATION QALIFICATIONS -:</h1>
   </div>
-  <p className='mb-2'>high school: {resume?.high}</p>
-  <p className='mb-2' >Intermediate: {resume?.inter}</p>
-  <p>graduate: {resume?.graduate}</p>
+  <p className='mb-2 font-semibold'>high school:  <span>{resume?.high}</span> </p>
+  <p className='mb-2 font-semibold' >Intermediate: <span>{resume?.inter}</span></p>
+  <p className='mb-2 font-semibold' >graduate: <span>{resume?.graduate}</span></p>
 </div>
 
 <div className='mb-4'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }} >
-  <h1 className='text-md font-bold '>EXTRA QALIFICATION -:</h1>
+  <h1 className='text-md font-bold leading-tight'>EXTRA QALIFICATION -:</h1>
   </div>
   <p>{resume?.extra}</p>
 </div>
 
 <div className='mb-4'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className='text-md font-bold '>WORK EXPRIENCE -:</h1>
+  <h1 className='text-md font-bold leading-tight '>WORK EXPRIENCE -:</h1>
   </div>
   <p>{resume?.workexp}</p>
 </div>
 
 <div className='mb-6'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-5 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto mb-5 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className=" grid grid-cols-1 font-semibold sm:grid-cols-2 gap-y-1 gap-x-6">PERSONAL DETAILS -:</h1>
+  <h1 className=" grid grid-cols-1 leading-tight font-semibold sm:grid-cols-2 gap-y-1 gap-x-6">PERSONAL DETAILS -:</h1>
   </div>
-  <p>father name : {resume?.fathername}</p>
-  <p>mother name : {resume?.mother}</p>
-  <p>Mataril status : {resume?.status}</p>
-  <p>Date of birth : {resume?.date}</p>
-  <p>Gender : {resume?.gender}</p>
-  <p >Languages : {resume?.language}</p>
+  <p  className='mb-1 font-semibold' >father name : <span>{resume?.fathername}</span></p>
+  <p className='mb-1 font-semibold'>mother name :  <span>{resume?.mother}</span></p>
+  <p className='mb-1 font-semibold'>Mataril status : <span>{resume?.status}</span></p>
+  <p className='mb-1 font-semibold'>Date of birth : <span>{resume?.date}</span></p>
+  <p className='mb-1 font-semibold'>Gender : <span>{resume?.gender}</span></p>
+  <p className='mb-1 font-semibold'>Languages : <span>{resume?.language}</span> </p>
 </div>
 
 <div className='mb-8'>
-  <div className='flex items-center justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
+  <div className='flex py-2 px-3 justify-start p-2 rounded-4xl h-auto mb-2 sm:mb-3' style={{backgroundColor:"#ECECEC"
   }}>
-  <h1 className='text-md font-bold '>DECLARATION -:</h1>
+  <h1 className='text-md font-bold leading-tight '>DECLARATION -:</h1>
   </div>
   <p>I here by declared that all the above entioned statements are true & correct to the best of my Knowlage.</p>
 </div>
