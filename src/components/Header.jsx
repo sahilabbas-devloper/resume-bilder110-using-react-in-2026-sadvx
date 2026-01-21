a46cf703225d4240df43e86ce02e48ab732ef003
Sahil Abbas <sa9300432@gmail.com>
2026-01-21 23:13:29 +0530

    fixed headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
     src="https://static.vecteezy.com/system/resources/previews/028/561/891/non_2x/resume-dual-tone-icon-icon-for-your-website-mobile-presentation-and-logo-design-vector.jpg" alt="" />
     <h1 className='text-xl font-semibold '> ResumeGen </h1>
     
-    <Link to={"/Hero"} className='ml-24 text-md md:ml-200 text-gray-800 font-semibold hover:text-purple-800'>Hero</Link>
+    <Link to={"/Hero"} className='ml-20 text-md md:ml-200 text-gray-800 font-semibold hover:text-purple-800'>Hero</Link>
     <Link to={"/Resume"} className='ml-4 text-md text-gray-800 font-semibold hover:text-purple-800'>preview</Link>
     </div>
 
